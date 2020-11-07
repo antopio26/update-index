eca4c0f62ad7add338400482257bc755fe412e0a
antopio26 <antoniopio.maggio@gmail.com>
2020-11-07 11:36:07 +0100

    Read file

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
+const fs = require('fs');
 
 try {
-    const path = core.getInput('path');
+    const paths = core.getInput('paths');
     const payload = github.context.payload;
     const commit = payload.commits[0];
     const author = commit.author.name;
     const timestamp = commit.timestamp;
 
+    var index_path = path.join(path.dirname(paths[0]), 'index.js');
+    var index = fs.readFileSync(index_path);
+
     console.log(`Author name: ${author}`)
     console.log(`Date: ${timestamp}`)
     console.log(`Files: ${path}`)
+    console.log(`Index content: ${path}`)
 } catch (error) {
     core.setFailed(error.message);
 }
