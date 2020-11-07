9ea49e5307c74850ccf64e5353a6eff5a03d4075
antopio26 <antoniopio.maggio@gmail.com>
2020-11-07 10:58:14 +0100

    Test files

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const github = require('@actions/github');
 try {
     const path = core.getInput('path');
     const payload = github.context.payload;
-    console.log(`Event Payload: ${JSON.stringify(payload, undefined, 2)}`)
+    const commit = payload.commits[0];
+    const author = commit.author.name;
+    const timestamp = commit.timestamp;
+
+    console.log(`Author name: ${author}`)
+    console.log(`Date: ${timestamp}`)
+    console.log(`Files: ${path}`)
 } catch (error) {
     core.setFailed(error.message);
 }
